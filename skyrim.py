75ccc4e2672e25dde9ba937f4857b0da0920c875
haiduk <hwlee91@gmail.com>
2020-09-08 01:44:55 -0400

    trying to concat two csv files

--- skyrim.py
+++ skyrim.py
@@ -2,16 +2,21 @@ import pandas as pd
 from tkinter import *
 from PIL import ImageTk, Image
 import sqlite3
+import os
 
 root = Tk()
 root.title('skyrim pricing')
+
 
 # reading csv files for skyrim's pricing
 a1 = pd.read_csv('price-history-for-72850.csv')
 a2 = pd.read_csv('price-history-for-489830.csv')
 
+concat = pd.concat([a1, a2])
+
+
 # displaying two separate csv files
-label1 = Label(root, text=a1).grid(row=0, column=0)
-label2 = Label(root, text=a2).grid(row=0, column=1)
+label1 = Label(root, text=concat).grid(row=0, column=0)
+#label2 = Label(root, text=a2).grid(row=0, column=1)
 
 root.mainloop()
