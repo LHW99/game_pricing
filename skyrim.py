226c46311937d2a38da49eb851d040e326fb3d94
haiduk <hwlee91@gmail.com>
2020-09-07 22:27:12 -0400

    reading both csv files

--- skyrim.py
+++ skyrim.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from tkinter import *
 from PIL import ImageTk, Image
+import sqlite3
 
 root = Tk()
 root.title('skyrim pricing')
 
-df = pd.read_csv('price-history-for-72850.csv')
+# reading csv files for skyrim's pricing
+a1 = pd.read_csv('price-history-for-72850.csv')
+a2 = pd.read_csv('price-history-for-489830.csv')
 
-label = Label(root, text=df).pack()
+# displaying two separate csv files
+label1 = Label(root, text=a1).grid(row=0, column=0)
+label2 = Label(root, text=a2).grid(row=0, column=1)
 
 root.mainloop()
