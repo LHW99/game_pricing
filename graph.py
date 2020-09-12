ff96686053faaf1762a1769a24b5df6ba9d927df
haiduk <hwlee91@gmail.com>
2020-09-12 01:04:03 -0400

    trying to line graph

--- graph.py
+++ graph.py
@@ -1,4 +1,22 @@
 from tkinter import *
 import pandas as pd
 import csv
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+root = Tk()
+root.title("line chart")
+root.geometry("400x400")
+
+
+def graph():
+  with open('new.csv', 'r') as file:
+    plot = csv.reader(file, delimiter=',')
+
+    plt.hist(plot)
+    plt.show
+
+btn = Button(root, text='graph', command=graph)
+btn.pack()
+
+
+root.mainloop()
