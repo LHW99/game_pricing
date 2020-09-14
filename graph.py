2f56b5f017771a1138b93a66e30cc2bacb8b42ed
haiduk <hwlee91@gmail.com>
2020-09-14 02:44:26 -0400

    pandas/numpy

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pandas as pd
+import datetime
 import csv
 # import matplotlib to graph
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ root = Tk()
 root.title("line chart")
 root.geometry("400x400")
 
+# tried graphing method
 """
 # make graph
 def graph():
@@ -25,6 +27,9 @@ btn.pack()
 root.mainloop()
 """
 
+
+#tried plt
+"""
 with open ('new.csv', 'r') as file:
   plot=pd.read_csv(file, delimiter=',')
 
@@ -32,4 +37,15 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('testing')
 plt.plot(plot)
+plt.show()
+"""
+
+# trying pandas/numpy
+headers = ['DateTime', 'Final price']
+df = pd.read_csv('new.csv', parse_dates = {"Datetime" : [1,2]}, names=headers)
+print (df)
+
+df.plot(x='DateTime', y='Final price')
+plt.title('title')
+plt.xticks(xValues)
 plt.show()
