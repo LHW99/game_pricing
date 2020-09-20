b7ba2283dc96cd96b817cd93e5e9dce99acfc229
haiduk <hwlee91@gmail.com>
2020-09-19 20:36:56 -0400

    dropdown

--- stats.py
+++ stats.py
@@ -5,7 +5,9 @@ root = Tk()
 root.title("Game Price Stats")
 root.geometry('600x600')
 
-# a dropdown box
+dat = ""
+
+# selections for dropdown
 options = [
   '1',
   '2',
@@ -13,7 +15,33 @@ options = [
   '4'
 ]
 
-btn=Button(root, text="click here")
+clicked = StringVar()
+clicked.set(options[0])
+
+# a dropdown box
+drop = OptionMenu(root, clicked, *options)
+drop.pack()
+
+global lb
+lb = Label(root, text='').pack()
+
+def select():
+  if clicked.get() == '1':
+    lb.destroy()
+    lb = Label(root, text='test1').pack()
+  elif clicked.get() == '2':
+    lb.destroy()
+    lb = Label(root, text='test2').pack()
+  elif clicked.get() == '3':
+    lb.destroy()
+    lb = Label(root, text='test3').pack()
+  elif clicked.get() == '4':
+    lb.destroy()
+    lb = Label(root, text='test4').pack()
+
+
+# button to execute
+btn=Button(root, text="click here", command=select)
 btn.pack()
 
 root.mainloop()
